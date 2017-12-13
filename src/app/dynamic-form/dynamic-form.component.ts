4f4cb4e894db71feb219c74bd7f704ab57c77107
Uri Schor <uri@cavsystems.com>
2017-12-13 11:39:18 +0200

    disable "save" button if all fields are read-only

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -1,19 +1,25 @@
-import { Component, Input, Output, EventEmitter, OnInit, OnChanges }  from '@angular/core';
-import { FormGroup }                 from '@angular/forms';
-import { Subject } from 'rxjs/Subject';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  OnChanges
+} from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { Subject } from "rxjs/Subject";
 
-import { QuestionBase }              from './question-base';
+import { QuestionBase } from "./question-base";
 
-import { QuestionControlService } from './question-control.service'
-import { QuestionService } from './question.service'
+import { QuestionControlService } from "./question-control.service";
+import { QuestionService } from "./question.service";
 
 @Component({
-  selector: 'dynamic-form',
-  templateUrl: './dynamic-form.component.html',
-  providers: [ QuestionControlService, QuestionService ]
+  selector: "dynamic-form",
+  templateUrl: "./dynamic-form.component.html",
+  providers: [QuestionControlService, QuestionService]
 })
 export class DynamicFormComponent implements OnInit, OnChanges {
-
   @Input() formKey: string;
   @Input() validateOnBlur: boolean;
   @Input() formParameters: any = null;
@@ -22,13 +28,15 @@ export class DynamicFormComponent implements OnInit, OnChanges {
 
   @Output() onSaved = new EventEmitter<any>();
   @Output() onCancelled = new EventEmitter<any>();
-  
+
   questions: QuestionBase<any>[] = [];
   form: FormGroup;
 
   loadingQuestions: Promise<any>;
 
-  constructor(private service: QuestionControlService) { }
+  readonly: boolean;
+
+  constructor(private service: QuestionControlService) {}
 
   ngOnChanges() {
     this.initForm();
@@ -39,20 +47,32 @@ export class DynamicFormComponent implements OnInit, OnChanges {
   }
 
   onSubmit() {
-    this.service.save(this.formKey, this.formParameters, this.form.value).then(response => {
-      this.onSaved.emit(response);
-    });
+    this.service
+      .save(this.formKey, this.formParameters, this.form.value)
+      .then(response => {
+        this.onSaved.emit(response);
+      });
   }
 
   onCancel() {
     this.onCancelled.emit();
   }
 
-  initForm() : void {
-    this.loadingQuestions = this.service.getQuestions(this.formKey, this.formParameters);
+  initForm(): void {
+    this.loadingQuestions = this.service.getQuestions(
+      this.formKey,
+      this.formParameters
+    );
     this.loadingQuestions.then(response => {
       this.questions = response;
       this.form = this.service.toFormGroup(this.questions, this.formKey);
+      this.readonly = true;
+      for (let question of this.questions) {
+        if (!question.readonly) {
+          this.readonly = false;
+          break;
+        }
+      }
     });
   }
 }
